40650b3e119b29ca0088bd99cb9b56df80b60c2a
fangmingdong <fangmingdong0224@gmail.com>
2019-07-12 11:19:00 +0800

    init

---
+++ array/1.two-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  var ret = [];
+  for (i = 0; i < nums.length; i++) {
+    for (j = i + 1; i < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        console.log(i, j);
+        ret.push(i, j);
+        return ret;
+      }
+    }
+  }
+};
+
+var res = twoSum([2, 7, 11, 15], 9);
+console.log(res);
